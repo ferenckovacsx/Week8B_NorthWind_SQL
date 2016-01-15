43b604811fa8deeeb38530835062f3165a00fba8
Ferenc Kovacs <ferenckovacsx@gmail.com>
2016-01-15 11:25:04 +0100

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -3,7 +3,5 @@ SELECT
     SUM(Quantity * OrderDetails.UnitPrice * (1 - OrderDetails.Discount)) AS OrderTotal
 FROM
     OrderDetails
-LEFT JOIN
-    Products ON OrderDetails.ProductID = Products.ProductID
 GROUP BY OrderID
 ORDER BY OrderID
